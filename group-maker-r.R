f32b3f24b8394ae5bc90a77ac3dd53c817336719
cforgaci <claudiuforgaci@gmail.com>
2021-07-14 13:04:42 +0200

    Update script.

--- group-maker-r.R
+++ group-maker-r.R
@@ -10,15 +10,47 @@ students$gender <- as.factor(students$gender)
 # How many students should be in a group?
 group_size <- 2
 
-make_groups <- function(students, group_size, no_groups = 2) {
-  # Check arguments
-  if (group_size < 2 & no_groups < 2) {
-    stop("A group should have at least two members.\nAt least two groups are required.")
-  } else if (group_size < 2) {
-    stop("A group should have at least two members.")
-  } else if (no_groups < 2) {
-    stop("At least two groups are required.")
+check_gender_balance <- function(students) {
+  # Check the gender balance in groups
+  gender_distr <- students %>% 
+    group_by(group) %>%
+    count(gender) %>% 
+    ungroup() %>% 
+    arrange(gender) %>% 
+    group_by(gender) %>% 
+    mutate(diff = n - lag(n)) %>%
+    ungroup() %>% 
+    mutate(diff_l = if_else(abs(diff) > 1, TRUE, FALSE)) %>% 
+    summarise(sum(diff_l, na.rm = TRUE)) %>% 
+    pull
+  
+  if (gender_distr == 0) {
+    print("Gender distribution across groups: OK")
   }
+}
+
+check_expertise_diversity <- function(students) {
+  # Check the distribution of designers per group
+  expertise_distr <- students %>% 
+    group_by(group) %>% 
+    count(designer) %>%
+    mutate(no_designers = sum(designer)) %>% 
+    mutate(diff = n - lag(n)) %>%
+    ungroup() %>% 
+    mutate(diff_l = if_else(abs(diff) > 1, TRUE, FALSE)) %>% 
+    summarise(sum(diff_l, na.rm = TRUE)) %>% 
+    pull
+  
+  if (expertise_distr == 0) {
+    print("Expertise distribution across groups: OK")
+  }
+}
+
+make_groups <- function(students, group_size) {
+  # Check arguments
+  if (group_size < 2) {
+    stop("A group should have at least two members.")
+  } 
   
   # How many groups does a given group size result in?
   no_groups <- nrow(students) %/% group_size
@@ -29,18 +61,12 @@ make_groups <- function(students, group_size, no_groups = 2) {
     add_count(nationality, sort = TRUE) %>% 
     select(-n) %>% 
     # distribute students across groups
-    mutate(group = rep_len(paste0("group_", (1:no_groups)), nrow(.)))
+    mutate(group = rep_len(paste0("group_", (1:no_groups)), nrow(.))) %>% 
+    arrange(group)
   
-  # check the distribution of designers per group
-  students %>% 
-    group_by(group) %>% 
-    count(designer) %>% 
-    filter(designer == TRUE)
-  
-  # check the gender balance in groups
-  students %>% 
-    group_by(group) %>%
-    count(gender)
-  # or
-  table(students$group, students$gender)
+  print(students)
+  check_expertise_diversity(students)  
+  check_gender_balance(students)
 }
+
+make_groups(students, 2)
