b45c44fbec11f9372c465e2429fad0ed91da370c
cforgaci <claudiuforgaci@gmail.com>
2021-07-30 21:25:40 +0200

    Update check_expertise_diversity.R

--- R/check_expertise_diversity.R
+++ R/check_expertise_diversity.R
@@ -1,16 +1,32 @@
+#' Check Expertise Diversity Across Groups
+#'
+#' After groups have been made, check expertise distribution.
+#'
+#' @param students A data frame.
+#'
+#' @return Return a message and the value TRUE if gender distribution is OK, FALSE otherwise.
+#'
+#' @import dplyr
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+#' @examples
+#' # Check expertise diversity on the example dataset included in the makegroups package
+#' check_expertise_diversity(make_groups(students, 3))
 check_expertise_diversity <- function(students) {
   # Check the distribution of designers per group
   expertise_distr <- students %>%
     # Count the number of designers in each group
-    dplyr::group_by(group) %>%
-    dplyr::count(designer) %>%
-    dplyr::mutate(no_designers = sum(designer)) %>%
+    dplyr::group_by(.data, .data$group) %>%
+    dplyr::count(.data$designer) %>%
+    dplyr::mutate(no_designers = sum(.data$designer)) %>%
     # Calculate the difference between groups
-    dplyr::mutate(diff = n - lag(n)) %>%
+    dplyr::mutate(diff = .data$n - lag(.data$n)) %>%
     dplyr::ungroup() %>%
     # Check if the difference is more than 1
-    dplyr::mutate(diff_l = if_else(abs(diff) > 1, TRUE, FALSE)) %>%
-    dplyr::summarise(sum(diff_l, na.rm = TRUE)) %>%
+    dplyr::mutate(diff_l = if_else(abs(.data$diff) > 1, TRUE, FALSE)) %>%
+    dplyr::summarise(sum(.data$diff_l, na.rm = TRUE)) %>%
     dplyr::pull()
 
   # If any group has more than one designer compared to other groups
